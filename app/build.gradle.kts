a894dd414b6db2716988cb70a736c29f6b8aef43
Mlevngr <z.shen1@tue.nl>
2026-08-15 21:03:11 +0200

    chore: prepare 0.6.8 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mlevngr.inknote"
         minSdk = 28
         targetSdk = 36
-        versionCode = 26
-        versionName = "0.6.7"
+        versionCode = 27
+        versionName = "0.6.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
