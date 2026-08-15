44b41f49462220131c9428d7ef8d5efc745b4024
Mlevngr <z.shen1@tue.nl>
2026-08-16 00:18:45 +0200

    chore: prepare 0.7.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mlevngr.inknote"
         minSdk = 28
         targetSdk = 36
-        versionCode = 31
-        versionName = "0.7.3"
+        versionCode = 32
+        versionName = "0.7.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
