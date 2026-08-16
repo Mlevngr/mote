22b82869feac94188fe92f955a7005ae103a6636
Mlevngr <z.shen1@tue.nl>
2026-08-16 09:43:14 +0200

    chore: prepare 0.7.9 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mlevngr.inknote"
         minSdk = 28
         targetSdk = 36
-        versionCode = 36
-        versionName = "0.7.8"
+        versionCode = 37
+        versionName = "0.7.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
